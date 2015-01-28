2eb8e186a8cc8fe4f8bfba7285c27b06b8a7ca13
Stephanie Burns <stephanie.burns@mac.com>
2015-01-28 14:32:08 +0800

    created all initial forms

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,40 @@
+class PurchasesController < ApplicationController
+
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+
+
+# These go together
+	def new
+		
+	end
+
+	def create
+		
+	end
+# Like ramalama ding dong
+
+
+# These go together
+	def edit
+
+	end
+
+	def update
+		
+	end
+# Like ramalama ding dong
+
+
+	def destroy
+		
+	end
+
+
+end
